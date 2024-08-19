62c984810e25747a769b4c56653c98d6f1097092
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-08-19 19:25:18 +0300

    update command

--- app/Console/Commands/DownloadPresentations.php
+++ app/Console/Commands/DownloadPresentations.php
@@ -26,16 +26,14 @@ class DownloadPresentations extends Command
     {
         info('START');
         // Retrieve all meetings
-        $meetings = AllRecording::query()->inRandomOrder()->limit(3)->get();
-        info($meetings);
-
+        $meetings = AllRecording::query()->inRandomOrder()->where('is_backed_up' , 0)->get();
         foreach ($meetings as $meeting) {
             $meeting_id = $meeting->record_id;
 
             // Prepare the Python script command
             $command = [
                 'python',
-                base_path('bbb-download/scripts/download_presentation.py'),
+                base_path('../player.academy-uk.net/scripts/download_presentation.py'),
                 'https://bbb.academy-uk.net',
                 $meeting_id
             ];
@@ -54,8 +52,8 @@ class DownloadPresentations extends Command
             $meeting->save();
             // Output the result of the command
             info("Downloaded presentation for meeting ID: {$meeting_id}");
+            info('END');
         }
-        info('END');
 
         return 0;
     }
